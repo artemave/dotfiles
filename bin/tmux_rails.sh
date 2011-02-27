f347b4ddebf80ba6f84999ad793240f397bdde47
artemave <artemave@gmail.com>
2011-02-27 18:13:04 +0000

    tmux rails conf works

--- bin/tmux_rails.sh
+++ bin/tmux_rails.sh
@@ -4,14 +4,14 @@ tmux start-server
 
 project_name=$(basename `pwd`)
 
-if [ ! $(tmux has-session -t $project_name) ] && [ -f ./config/routes.rb ]; then
+if ! $(tmux has-session -t $project_name) && [ -f ./config/routes.rb ]; then
   tmux new-session -d -s $project_name -n shell
 
   tmux new-window -t $project_name:2 -n vim vim
   tmux new-window -t $project_name:3 -n console 'rails c'
-  tmux split-window -t $project_name:3 -h 'rails db'
+  tmux split-window -t $project_name:3 -h 'rails db -p'
   tmux new-window -t $project_name:4 -n server-log 'rails s'
-  tmux split-window -t $project_name:4 'tail -f log/development.log'
+  tmux split-window -t $project_name:4 'tail -f ./log/development.log'
 
   tmux select-window -t $project_name:1
 fi
