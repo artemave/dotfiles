22b91c2edb16efb7ef3da2bf9bb2740cf2efc36e
artemave <artemave@gmail.com>
2011-06-05 16:55:29 +0100

    turns out FileUtils has :verbose => true option which is handy

--- install.rb
+++ install.rb
@@ -7,26 +7,21 @@ BACKUP_DIR = File.join Dir.tmpdir, Time.now.to_i.to_s, 'myrcs'
 HOME_DIR = Dir.respond_to?(:home) ? Dir.home : ENV['HOME']
 
 def backup(file)
-  FileUtils.mkdir_p BACKUP_DIR
-
   basename = File.basename file
   existing_file = File.join HOME_DIR, basename
   backup_copy = File.join BACKUP_DIR, basename
   
   if File.exists? existing_file
     if File.symlink? existing_file
-      puts "Removing old link: #{existing_file}"
-      FileUtils.rm existing_file
+      FileUtils.rm existing_file, :verbose => true
     else
-      puts "Backing up: mv #{existing_file} #{backup_copy}"
-      FileUtils.mv existing_file, backup_copy
+      FileUtils.mv existing_file, backup_copy, :verbose => true
     end
   end
 end
 
 def link_to_home(file)
-  puts "ln -s #{file} ~/"
-  FileUtils.ln_s file, HOME_DIR
+  FileUtils.ln_s file, HOME_DIR, :verbose => true
 end
 
 def snippify
@@ -34,18 +29,22 @@ def snippify
   more_snippets_path = File.join(HOME_DIR, '.vim', 'bundle', 'snipmate-snippets', 'snippets')
   if File.exists?(snipmate_path) and File.exists?(more_snippets_path)
     puts 'Installing more cool snippets...'
-    FileUtils.rm_rf "#{snipmate_path}/snippets"
-    FileUtils.cp "#{more_snippets_path}/support_functions.vim", "#{snipmate_path}/plugin"
-    #FileUtils.ln_s more_snippets_path, snippets_path
+    FileUtils.rm_rf "#{snipmate_path}/snippets", :verbose => true
+    FileUtils.cp "#{more_snippets_path}/support_functions.vim", "#{snipmate_path}/plugin", :verbose => true
+    #FileUtils.ln_s more_snippets_path, snippets_path, :verbose => true
     puts 'Done with snippets.'
   else
     puts 'No cool vim snippets. Skipping.'
   end
 end
 
-`git submodule update --init`
+puts 'Updating git submodules...'
+puts `git submodule update --init`
+puts 'Done!'
 
 puts "Installing dot files..."
+
+FileUtils.mkdir_p BACKUP_DIR, :verbose => true
 
 Dir.glob("#{Dir.pwd}/*", File::FNM_DOTMATCH).each do |f|
   next if f =~ /\.$|\.\.|.git$|.swp$|.gitmodules|install.rb/
