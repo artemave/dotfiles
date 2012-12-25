9e98f7b1b566e850eec709c7c3bed37b2280bf95
artemave <artemave@gmail.com>
2012-12-25 20:00:40 +0000

    reanimate tmux_rails

--- bin/tmux_rails.sh
+++ bin/tmux_rails.sh
@@ -6,15 +6,13 @@ project_name=$(basename `pwd`)
 
 if ! $(tmux has-session -t $project_name) && [ -f ./config/routes.rb ]; then
   tmux new-session -d -s $project_name -n shell
-  tmux set-option -t $project_name set-remain-on-exit on
-
   tmux new-window -t $project_name:2 -n vim vim
-  tmux new-window -t $project_name:3 -n console 'bundle exec rails c'
-  tmux split-window -t $project_name:3 'bundle exec rails db -p'
-  tmux new-window -t $project_name:4 -n server-log 'bundle exec rails s'
-  tmux split-window -t $project_name:4 'tail -f ./log/development.log'
+  tmux new-window -t $project_name:3 -n zeus #'zeus start'
+  tmux new-window -t $project_name:4 -n server
+  tmux new-window -t $project_name:5 -n console
+  tmux new-window -t $project_name:6 -n logs
 
   tmux select-window -t $project_name:1
 fi
 
-tmux -u attach-session -t $project_name
+#tmux -u attach-session -t $project_name
