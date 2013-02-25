0a0833a658f0973a8b35aa88535fb8912c04e1b0
artemave <artemave@gmail.com>
2013-02-25 12:21:46 +0000

    tmux rails: do not start vim or else window closes on vim exit

--- bin/tmux_rails.sh
+++ bin/tmux_rails.sh
@@ -6,7 +6,7 @@ project_name=$(basename `pwd`)
 
 if ! $(tmux has-session -t $project_name) && [ -f ./config/routes.rb ]; then
   tmux new-session -d -s $project_name -n shell
-  tmux new-window -t $project_name:2 -n vim vim
+  tmux new-window -t $project_name:2 -n vim
   tmux new-window -t $project_name:3 -n zeus #'zeus start'
   tmux new-window -t $project_name:4 -n server
   tmux new-window -t $project_name:5 -n console
