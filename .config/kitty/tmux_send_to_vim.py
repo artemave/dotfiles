b4d94793aade7acaffd5964330cf53990ca87689
artemave <artemave@gmail.com>
2019-11-20 19:09:18 +0100

    Make sure switching between tmux session doesn't break my hints

--- .config/kitty/tmux_send_to_vim.py
+++ .config/kitty/tmux_send_to_vim.py
@@ -2,7 +2,7 @@
 
 import re
 import os
-from kittens.tui.loop import debug
+import subprocess
 
 def camel_to_snake(string):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
@@ -12,6 +12,10 @@ def mark(text, args, Mark, extra_cli_args, *a):
     # This function is responsible for finding all
     # matching text.
     # We mark all individual word for potential selection
+
+    current_tmux_session_path = subprocess.run( 'tmux display-message -p -F "#{pane_current_path}" -t0'.split(' '), stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
+    # for some reason the output of the above comes back in double quotes
+    current_tmux_session_path = re.sub(r'^"|"$', '', current_tmux_session_path)
 
     regexp = re.compile(
             '(?P<rails_log_controller>(?:[A-Z]\\w*::)*[A-Z]\\w*Controller#\\w+)|'
@@ -40,7 +44,7 @@ def mark(text, args, Mark, extra_cli_args, *a):
             if len(parts) > 1:
                 line_number = parts[1]
 
-            if file_path != '.' and file_path != '..' and file_path != '/' and os.path.exists(file_path):
+            if file_path != '.' and file_path != '..' and file_path != '/' and os.path.exists(os.path.join(current_tmux_session_path, file_path)):
                 mark_data = {
                         'file_path': file_path,
                         'line_number': line_number
@@ -65,7 +69,7 @@ def mark(text, args, Mark, extra_cli_args, *a):
                     ) + '.rb'
             method_def_regex = re.compile('^\\s*def\\s+%s' % (action))
 
-            if os.path.exists(controller_path):
+            if os.path.exists(os.path.join(current_tmux_session_path, controller_path)):
                 with open(controller_path) as ruby_file:
                     line_number = 0
                     for line in ruby_file:
@@ -113,6 +117,7 @@ def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
             os.system('tmux send-keys zz')
 
 # if __name__ == "__main__":
+#     from kittens.tui.loop import debug
 #     cwd = os.getcwd()
 #     debug({'cwd': cwd})
 
@@ -120,83 +125,71 @@ if __name__ == "change to __main__ when running this directly":
     from pprint import pprint
 
     text = """
-Completed 200 OK in 213ms (Views: 173.1ms | ActiveRecord: 18.7ms | Allocations: 40616)
+Rendered layouts/_base.html.erb (Duration: 32.9ms | Allocations: 2204)
+Completed 200 OK in 367ms (Views: 316.5ms | ActiveRecord: 23.2ms | Allocations: 40554)
 
 
-Started GET "/en/checkout/shipping_addresses/new" for ::1 at 2019-11-12 16:39:35 +0100
-Processing by CheckoutShippingAddressesController#new as HTML
-  Parameters: {"locale"=>"en"}
-  Client Load (0.4ms)  SELECT "clients".* FROM "clients" WHERE "clients"."subdomain" = $1 LIMIT $2  [["subdomain", "zams"], ["LIMIT", 1]]
+Started GET "/en/orders/1007056676" for ::1 at 2019-11-18 12:49:58 +0100
+Processing by OrdersController#show as HTML
+  Parameters: {"locale"=>"en", "id"=>"1007056676"}
+  Client Load (1.4ms)  SELECT "clients".* FROM "clients" WHERE "clients"."subdomain" = $1 LIMIT $2  [["subdomain", "zams"], ["LIMIT", 1]]
   ↳ app/controllers/concerns/tenantable.rb:22:in `current_tenant_is_client_with'
-  User Load (0.5ms)  SELECT "users".* FROM "users" WHERE "users"."client_id" = $1 AND "users"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 80395490], ["LIMIT", 1]]
-  ↳ app/controllers/client_base_controller.rb:52:in `current_user'
-  Page Load (1.4ms)  SELECT slug, title FROM "pages" WHERE "pages"."client_id" = $1  [["client_id", 898370464]]
+  User Load (1.0ms)  SELECT "users".* FROM "users" WHERE "users"."client_id" = $1 AND "users"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 599798772], ["LIMIT", 1]]
+  ↳ app/controllers/client_base_controller.rb:48:in `current_user'
+  Page Load (1.8ms)  SELECT slug, title FROM "pages" WHERE "pages"."client_id" = $1  [["client_id", 898370464]]
   ↳ app/models/page.rb:19:in `all_slugs_and_titles'
-  Basket Load (0.6ms)  SELECT "baskets".* FROM "baskets" WHERE "baskets"."client_id" = $1 AND "baskets"."user_id" = $2 LIMIT $3  [["client_id", 898370464], ["user_id", 80395490], ["LIMIT", 1]]
-  ↳ app/controllers/concerns/basketable.rb:14:in `current_basket'
-  ClientConfig Load (0.5ms)  SELECT "client_configs".* FROM "client_configs" WHERE "client_configs"."client_id" = $1 ORDER BY "client_configs"."id" DESC LIMIT $2  [["client_id", 898370464], ["LIMIT", 1]]
+  Order Load (0.6ms)  SELECT "orders".* FROM "orders" WHERE "orders"."client_id" = $1 AND "orders"."user_id" = $2 AND "orders"."id" = $3 LIMIT $4  [["client_id", 898370464], ["user_id", 599798772], ["id", 1007056676], ["LIMIT", 1]]
+  ↳ app/controllers/orders_controller.rb:5:in `show'
+  Rendering orders/show.html.erb within layouts/application
+  LineItem Load (2.0ms)  SELECT "line_items".* FROM "line_items" INNER JOIN "line_item_groups" ON "line_items"."line_item_group_id" = "line_item_groups"."id" WHERE "line_items"."client_id" = $1 AND "line_item_groups"."client_id" = $2 AND "line_item_groups"."order_id" = $3  [["client_id", 898370464], ["client_id", 898370464], ["order_id", 1007056676]]
+  ↳ app/models/order.rb:30:in `number_of_items'
+  LineItemGroup Load (0.4ms)  SELECT "line_item_groups".* FROM "line_item_groups" WHERE "line_item_groups"."client_id" = $1 AND "line_item_groups"."order_id" = $2  [["client_id", 898370464], ["order_id", 1007056676]]
+  ↳ app/models/order.rb:38:in `pending_approval?'
+  VetPrescription Load (0.3ms)  SELECT "vet_prescriptions".* FROM "vet_prescriptions" WHERE "vet_prescriptions"."client_id" = $1 AND "vet_prescriptions"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 1029531331], ["LIMIT", 1]]
+  ↳ app/models/line_item_group.rb:21:in `effective_strategy'
+  LineItem Load (1.2ms)  SELECT "line_items".* FROM "line_items" WHERE "line_items"."client_id" = $1 AND "line_items"."line_item_group_id" = $2  [["client_id", 898370464], ["line_item_group_id", 1060584122]]
+  ↳ app/views/partials/_order_vet_prescription_table.html.erb:11
+  Product Load (1.0ms)  SELECT "products".* FROM "products" WHERE "products"."id" = $1 LIMIT $2  [["id", 979904108], ["LIMIT", 1]]
+  ↳ app/models/line_item.rb:15:in `image_url'
+  Rendered partials/_order_vet_prescription_table.html.erb (Duration: 26.7ms | Allocations: 3815)
+  VetPrescriptionDetail Load (1.4ms)  SELECT "vet_prescription_details".* FROM "vet_prescription_details" WHERE "vet_prescription_details"."client_id" = $1 AND "vet_prescription_details"."vet_prescription_id" = $2 LIMIT $3  [["client_id", 898370464], ["vet_prescription_id", 1029531331], ["LIMIT", 1]]
+  ↳ app/views/partials/_vet_prescription_summary.html.erb:1
+  Rendered partials/_vet_prescription_summary.html.erb (Duration: 18.3ms | Allocations: 2070)
+  Rendered orders/_vet_prescription.html.erb (Duration: 50.7ms | Allocations: 7448)
+  ShippingAddress Load (0.5ms)  SELECT "addresses".* FROM "addresses" WHERE "addresses"."client_id" = $1 AND "addresses"."type" = $2 AND "addresses"."id" = $3 LIMIT $4  [["client_id", 898370464], ["type", "ShippingAddress"], ["id", 959340445], ["LIMIT", 1]]
+  ↳ app/views/orders/_order_details.html.erb:25
+  Dispatch Exists? (0.8ms)  SELECT 1 AS one FROM "dispatches" INNER JOIN "line_item_groups" ON "dispatches"."line_item_group_id" = "line_item_groups"."id" WHERE "dispatches"."client_id" = $1 AND "line_item_groups"."client_id" = $2 AND "line_item_groups"."order_id" = $3 LIMIT $4  [["client_id", 898370464], ["client_id", 898370464], ["order_id", 1007056676], ["LIMIT", 1]]
+  ↳ app/views/orders/_order_details.html.erb:33
+  Dispatch Load (0.7ms)  SELECT "dispatches".* FROM "dispatches" INNER JOIN "line_item_groups" ON "dispatches"."line_item_group_id" = "line_item_groups"."id" WHERE "dispatches"."client_id" = $1 AND "line_item_groups"."client_id" = $2 AND "line_item_groups"."order_id" = $3  [["client_id", 898370464], ["client_id", 898370464], ["order_id", 1007056676]]
+  ↳ app/views/orders/_order_details.html.erb:34:in `sort'
+  DispatchStatusUpdate Load (0.6ms)  SELECT "dispatch_status_updates".* FROM "dispatch_status_updates" WHERE "dispatch_status_updates"."client_id" = $1 AND "dispatch_status_updates"."dispatch_id" = $2 ORDER BY id  [["client_id", 898370464], ["dispatch_id", 305891094]]
+  ↳ app/views/orders/_order_details.html.erb:44
+  Payment Load (0.5ms)  SELECT "payments".* FROM "payments" WHERE "payments"."client_id" = $1 AND "payments"."order_id" = $2  [["client_id", 898370464], ["order_id", 1007056676]]
+  ↳ app/views/orders/_order_details.html.erb:63
+  Card Load (1.9ms)  SELECT "cards".* FROM "cards" WHERE "cards"."client_id" = $1 AND "cards"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 1037122605], ["LIMIT", 1]]
+  ↳ app/views/orders/_order_details.html.erb:77
+  Rendered orders/_order_details.html.erb (Duration: 111.7ms | Allocations: 22207)
+  Rendered orders/show.html.erb within layouts/application (Duration: 151.2ms | Allocations: 27021)
+  ClientConfig Load (0.6ms)  SELECT "client_configs".* FROM "client_configs" WHERE "client_configs"."client_id" = $1 ORDER BY "client_configs"."id" DESC LIMIT $2  [["client_id", 898370464], ["LIMIT", 1]]
   ↳ app/models/client_config.rb:53:in `entity'
-  Rendering checkout_shipping_addresses/new.html.erb within layouts/application
-  Rendered partials/_form_errors.html.erb (Duration: 0.1ms | Allocations: 16)
-  Rendered partials/_shipping_address_fields.html.erb (Duration: 21.7ms | Allocations: 2760)
-  ShippingAddress Exists? (0.6ms)  SELECT 1 AS one FROM "addresses" WHERE "addresses"."client_id" = $1 AND "addresses"."type" = $2 AND "addresses"."user_id" = $3 LIMIT $4  [["client_id", 898370464], ["type", "ShippingAddress"], ["user_id", 80395490], ["LIMIT", 1]]
-  ↳ app/views/checkout_shipping_addresses/new.html.erb:20
-  BasketItemGroup Load (0.5ms)  SELECT "basket_item_groups".* FROM "basket_item_groups" WHERE "basket_item_groups"."client_id" = $1 AND "basket_item_groups"."basket_id" = $2  [["client_id", 898370464], ["basket_id", 342761]]
-  ↳ app/models/basket.rb:48:in `all?'
-  VetPrescription Load (1.1ms)  SELECT "vet_prescriptions".* FROM "vet_prescriptions" WHERE "vet_prescriptions"."client_id" = $1 AND "vet_prescriptions"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 1029531330], ["LIMIT", 1]]
-  ↳ app/models/basket.rb:48:in `block in all_items_ungrouped?'
-  ContactLensPrescription Load (0.5ms)  SELECT "contact_lens_prescriptions".* FROM "contact_lens_prescriptions" WHERE "contact_lens_prescriptions"."client_id" = $1 AND "contact_lens_prescriptions"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 447876413], ["LIMIT", 1]]
-  ↳ app/models/basket.rb:107:in `map'
-  VetPrescriptionDetail Load (0.5ms)  SELECT "vet_prescription_details".* FROM "vet_prescription_details" WHERE "vet_prescription_details"."client_id" = $1 AND "vet_prescription_details"."vet_prescription_id" = $2 LIMIT $3  [["client_id", 898370464], ["vet_prescription_id", 1029531330], ["LIMIT", 1]]
-  ↳ app/models/vet_prescription.rb:66:in `checkoutable?'
-  ContactLensPrescriptionDetail Load (0.6ms)  SELECT "contact_lens_prescription_details".* FROM "contact_lens_prescription_details" WHERE "contact_lens_prescription_details"."client_id" = $1 AND "contact_lens_prescription_details"."contact_lens_prescription_id" = $2 LIMIT $3  [["client_id", 898370464], ["contact_lens_prescription_id", 447876413], ["LIMIT", 1]]
-  ↳ app/models/contact_lens_prescription.rb:45:in `checkoutable?'
-  Rendered partials/_checkout_accordion_basket_items.html.erb (Duration: 37.9ms | Allocations: 11096)
-  Card Load (1.3ms)  SELECT "cards".* FROM "cards" WHERE "cards"."client_id" = $1 AND "cards"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 141474312], ["LIMIT", 1]]
-  ↳ app/views/partials/_checkout_accordion_payment.html.erb:1
-  Rendered partials/_checkout_accordion_payment.html.erb (Duration: 19.3ms | Allocations: 3743)
-  Rendered partials/_checkout_accordion_place_order.html.erb (Duration: 1.8ms | Allocations: 400)
-Completed 200 OK in 213ms (Views: 173.1ms | ActiveRecord: 18.7ms | Allocations: 40616)
-
-
-Started GET "/en/checkout/shipping_addresses/new" for ::1 at 2019-11-12 16:39:35 +0100
-Processing by CheckoutShippingAddressesController#new as HTML
-  Parameters: {"locale"=>"en"}
-  Client Load (0.4ms)  SELECT "clients".* FROM "clients" WHERE "clients"."subdomain" = $1 LIMIT $2  [["subdomain", "zams"], ["LIMIT", 1]]
-  ↳ app/controllers/concerns/tenantable.rb:22:in `current_tenant_is_client_with'
-  User Load (0.5ms)  SELECT "users".* FROM "users" WHERE "users"."client_id" = $1 AND "users"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 80395490], ["LIMIT", 1]]
-  ↳ app/controllers/client_base_controller.rb:52:in `current_user'
-  Page Load (1.4ms)  SELECT slug, title FROM "pages" WHERE "pages"."client_id" = $1  [["client_id", 898370464]]
-  ↳ app/models/page.rb:19:in `all_slugs_and_titles'
-  Basket Load (0.6ms)  SELECT "baskets".* FROM "baskets" WHERE "baskets"."client_id" = $1 AND "baskets"."user_id" = $2 LIMIT $3  [["client_id", 898370464], ["user_id", 80395490], ["LIMIT", 1]]
+  Basket Load (0.4ms)  SELECT "baskets".* FROM "baskets" WHERE "baskets"."client_id" = $1 AND "baskets"."user_id" = $2 LIMIT $3  [["client_id", 898370464], ["user_id", 599798772], ["LIMIT", 1]]
   ↳ app/controllers/concerns/basketable.rb:14:in `current_basket'
-  ClientConfig Load (0.5ms)  SELECT "client_configs".* FROM "client_configs" WHERE "client_configs"."client_id" = $1 ORDER BY "client_configs"."id" DESC LIMIT $2  [["client_id", 898370464], ["LIMIT", 1]]
+  CACHE ClientConfig Load (0.1ms)  SELECT "client_configs".* FROM "client_configs" WHERE "client_configs"."client_id" = $1 ORDER BY "client_configs"."id" DESC LIMIT $2  [["client_id", 898370464], ["LIMIT", 1]]
   ↳ app/models/client_config.rb:53:in `entity'
-  Rendering checkout_shipping_addresses/new.html.erb within layouts/application
-  Rendered partials/_form_errors.html.erb (Duration: 0.1ms | Allocations: 16)
-  Rendered partials/_shipping_address_fields.html.erb (Duration: 21.7ms | Allocations: 2760)
-  ShippingAddress Exists? (0.6ms)  SELECT 1 AS one FROM "addresses" WHERE "addresses"."client_id" = $1 AND "addresses"."type" = $2 AND "addresses"."user_id" = $3 LIMIT $4  [["client_id", 898370464], ["type", "ShippingAddress"], ["user_id", 80395490], ["LIMIT", 1]]
-  ↳ app/views/checkout_shipping_addresses/new.html.erb:20
-  BasketItemGroup Load (0.5ms)  SELECT "basket_item_groups".* FROM "basket_item_groups" WHERE "basket_item_groups"."client_id" = $1 AND "basket_item_groups"."basket_id" = $2  [["client_id", 898370464], ["basket_id", 342761]]
-  ↳ app/models/basket.rb:48:in `all?'
-  VetPrescription Load (1.1ms)  SELECT "vet_prescriptions".* FROM "vet_prescriptions" WHERE "vet_prescriptions"."client_id" = $1 AND "vet_prescriptions"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 1029531330], ["LIMIT", 1]]
-  ↳ app/models/basket.rb:48:in `block in all_items_ungrouped?'
-  ContactLensPrescription Load (0.5ms)  SELECT "contact_lens_prescriptions".* FROM "contact_lens_prescriptions" WHERE "contact_lens_prescriptions"."client_id" = $1 AND "contact_lens_prescriptions"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 447876413], ["LIMIT", 1]]
-  ↳ app/models/basket.rb:107:in `map'
-  VetPrescriptionDetail Load (0.5ms)  SELECT "vet_prescription_details".* FROM "vet_prescription_details" WHERE "vet_prescription_details"."client_id" = $1 AND "vet_prescription_details"."vet_prescription_id" = $2 LIMIT $3  [["client_id", 898370464], ["vet_prescription_id", 1029531330], ["LIMIT", 1]]
-  ↳ app/models/vet_prescription.rb:66:in `checkoutable?'
-  ContactLensPrescriptionDetail Load (0.6ms)  SELECT "contact_lens_prescription_details".* FROM "contact_lens_prescription_details" WHERE "contact_lens_prescription_details"."client_id" = $1 AND "contact_lens_prescription_details"."contact_lens_prescription_id" = $2 LIMIT $3  [["client_id", 898370464], ["contact_lens_prescription_id", 447876413], ["LIMIT", 1]]
-  ↳ app/models/contact_lens_prescription.rb:45:in `checkoutable?'
-  Rendered partials/_checkout_accordion_basket_items.html.erb (Duration: 37.9ms | Allocations: 11096)
-  Card Load (1.3ms)  SELECT "cards".* FROM "cards" WHERE "cards"."client_id" = $1 AND "cards"."id" = $2 LIMIT $3  [["client_id", 898370464], ["id", 141474312], ["LIMIT", 1]]
-  ↳ app/views/partials/_checkout_accordion_payment.html.erb:1
-  Rendered partials/_checkout_accordion_payment.html.erb (Duration: 19.3ms | Allocations: 3743)
-  Rendered partials/_checkout_accordion_place_order.html.erb (Duration: 1.8ms | Allocations: 400)
-  http://localhost:3000 tcp://localhost:5000 https://www.google.com/s?q=stuff
+  Rendered partials/_client_user_bar.html.erb (Duration: 22.6ms | Allocations: 5429)
+  ActiveStorage::Attachment Load (0.3ms)  SELECT "active_storage_attachments".* FROM "active_storage_attachments" WHERE "active_storage_attachments"."record_id" = $1 AND "active_storage_attachments"."record_type" = $2 AND "active_storage_attachments"."name" = $3 LIMIT $4  [["record_id", 906572757], ["record_type", "ClientConfig"], ["name", "logo_image"], ["LIMIT", 1]]
+  ↳ app/views/partials/_client_logo.html.erb:2
+  Rendered partials/_client_logo.html.erb (Duration: 7.5ms | Allocations: 1672)
+  Rendered partials/_client_logo_bar.html.erb (Duration: 10.3ms | Allocations: 2232)
+  Rendered partials/_flash.html.erb (Duration: 2.9ms | Allocations: 186)
+  Rendered partials/_current_theme.html.erb (Duration: 1.5ms | Allocations: 587)
+  Rendered layouts/_base.html.erb (Duration: 40.7ms | Allocations: 3116)
+Completed 200 OK in 307ms (Views: 256.6ms | ActiveRecord: 17.5ms | Allocations: 47708)
+
     """
 
     def ap(idx, start, end, mark_text, groupdicts):
-        pprint.pprint({
+        pprint({
             'idx': idx,
             'start': start,
             'end': end,
