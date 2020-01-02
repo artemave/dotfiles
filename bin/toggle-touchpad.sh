f73298bca7f2412977163e7115a51a61eed28ae3
artemave <artemave@gmail.com>
2020-01-02 22:13:53 +0100

    Add key mapping to toggle touchpad

---
+++ bin/toggle-touchpad.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+#!/bin/bash
+
+device=11
+state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
+
+if [ $state == '1' ];then
+  xinput --disable "$device"
+else
+  xinput --enable "$device"
+fi
