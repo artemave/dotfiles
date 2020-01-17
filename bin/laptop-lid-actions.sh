ac834a2882c0ba0bc6ddbef1908bbf3bc4e446e2
artemave <artemave@gmail.com>
2020-01-17 23:25:51 +0100

    Fix laptop lid close not turning off bluetooth

---
+++ bin/laptop-lid-actions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# rule is here: /etc/acpi/events/laptop-lid
+#
+# file contains:
+#
+# event=button/lid.*
+# action=/home/artem/bin/laptop-lid-actions.sh
+#
+# sudo service acpi restart
+
+if grep -q closed /proc/acpi/button/lid/LID/state; then
+  # close action
+  bluetooth off
+else
+  # open action
+  bluetooth on
+fi
