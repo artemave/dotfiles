f4b9a98a71144581e22fc7d581fac33e7836e267
artemave <artemave@gmail.com>
2023-09-06 14:29:27 +0200

    fix mic mute button

---
+++ .config/scripts/mic_mute.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+pactl set-source-mute @DEFAULT_SOURCE@ toggle
+
+mic_status=$(pactl get-source-mute @DEFAULT_SOURCE@ | cut -d ' ' -f2)
+if [ 'no' == "$mic_status" ]; then
+  brightnessctl -d platform::micmute s 0
+else
+  brightnessctl -d platform::micmute s 1
+fi
+
+pkill -RTMIN+1 i3blocks
