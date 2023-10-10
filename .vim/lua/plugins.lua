48a7fcdbd20f293e8e5765b771fa5d33be09c8a3
artemave <artemave@gmail.com>
2023-10-10 16:09:38 +0200

    mdx syntax highlight

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -38,7 +38,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gT', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -160,9 +160,10 @@ require'nvim-treesitter.configs'.setup {
   ignore_install = { "ledger", "gdscript", "supercollider", "devicetree", "nix", "erlang", "ocamllex" },
   highlight = {
     enable = true,              -- false will disable the whole extension
+    disable = { 'vimscript' } -- suddenly it's very slow
   },
   indent = {
-    enable = false,              -- false will disable the whole extension
+    enable = true,              -- false will disable the whole extension
   },
   incremental_selection = {
     enable = true,
@@ -258,6 +259,15 @@ require'nvim-treesitter.configs'.setup {
     },
   }
 }
+
+-- taken from https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/
+vim.filetype.add({
+  extension = {
+    mdx = 'mdx'
+  }
+})
+local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+ft_to_parser.mdx = "markdown"
 
 require('ts_context_commentstring').setup {}
 
