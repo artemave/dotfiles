4f54a45c9d360be54f1525cd4af6d95693bdfc8d
artemave <artemave@gmail.com>
2024-02-26 13:45:49 +0100

    Fix not suspending on lid close when external screen is attached

--- .config/xidlehook.sh
+++ .config/xidlehook.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-
-~/.cargo/bin/xidlehook \
+~/projects/xidlehook/target/release/xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
   --timer 300 'i3lock --color=4c7899 --ignore-empty-password --show-failed-attempts --nofork' '' \
